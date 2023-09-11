c0c60bf00543ef11e9c7107ad97f658b648f1ca4
sreejith98 <sreejithpnair143@gmail.com>
2023-09-12 02:00:02 +0530

    Quiz App Microservices

---
+++ quiz-service/src/main/java/com/microservice/quizservice/dao/QuizDao.java
@@ -0,0 +1,9 @@
+package com.microservice.quizservice.dao;
+
+import com.microservice.quizservice.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizDao extends JpaRepository<Quiz,Integer> {
+}
